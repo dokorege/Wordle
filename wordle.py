7c7bcb7b60a50513453caefd77c44d74f0c75e77
dokorege <107742790+dokorege@users.noreply.github.com>
2022-06-18 02:03:42 -0400

    Create wordle.py

---
+++ wordle.py
@@ -0,0 +1,47 @@
+from nltk.corpus import words
+import random
+from termcolor import colored
+
+wordList = []
+wordsVoid = set()
+for i in words.words():
+  if len(i) == 5:
+    wordList.append(i.upper())
+wordChoice = [i for i in random.choice(wordList).upper()]
+guess = 0 
+
+print("Here are the rules: \nGreen Letters means they're in the correct position\nYellow Letters are in the word...but in the wrong position\nWhite Letters are not in the word\nEmpty Guesses and Words not equal to 5 letters are not counted as guesses.\n")
+
+while guess <= 6:
+  guess += 1
+  userInput = input('Input Word Guess: ').upper()
+  userContent = [i for i in userInput]
+  emptyList = []
+
+  if userContent == wordChoice:
+    print(colored(f"{''.join(userContent)}", 'green'))
+    print(f'Correct, the word was {"".join(wordChoice)}. It took you {guess} Tries.')
+    break
+  if userInput in wordList:
+    for i in range(0, len(wordChoice)):
+      if userContent[i] in wordChoice: 
+        if userContent[i] == wordChoice[i]:
+          emptyList.append(colored(f'{userContent[i]}', 'green'))
+        else: 
+          emptyList.append(colored(f'{userContent[i]}', 'yellow'))
+      else:
+        emptyList.append(colored(f'{userContent[i]}', 'white'))
+        wordsVoid.add(userContent[i])
+  else: 
+    if len(userContent) != 5: 
+      print('Not a Five Letter Word\n')
+      guess -= 1
+      continue
+    if userInput not in wordList:
+      print('Not in Word List\n')
+      guess -= 1
+      continue
+  print(''.join(emptyList))
+  print(f'Letters not in word: {sorted(wordsVoid)}\n') 
+
+print(f'The Word Was {"".join(wordChoice).lower().capitalize()}') 
